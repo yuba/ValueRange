5f1079b015bf63cdf6fcad32cb68bdb816b25d63
Miura Takuma <mueller@inter7.jp>
2012-09-26 23:46:17 +0900

    SingleRange, ComplexRangeをクラス内クラスにして隠蔽

--- ValueRange/ComplexRange.cs
+++ ValueRange/ComplexRange.cs
@@ -5,79 +5,82 @@ using System.Text;
 
 namespace ValueRange
 {
-    public class ComplexRange<T> : Range<T> where T:IComparable<T>
+    public abstract partial class Range<T> where T : IComparable<T>
     {
-        private SingleRange<T>[] elements;
-
-        public ComplexRange(SingleRange<T>[] elements)
+        protected class ComplexRange : Range<T>
         {
-            this.elements = new SingleRange<T>[elements.Length];
-            Array.Copy(elements, this.elements, elements.Length);
-        }
+            private SingleRange[] elements;
 
-        public override bool Includes(T value)
-        {
-            return elements.Any(element => element.Includes(value));
-        }
+            public ComplexRange(SingleRange[] elements)
+            {
+                this.elements = new SingleRange[elements.Length];
+                Array.Copy(elements, this.elements, elements.Length);
+            }
 
-        public override bool OverlapsWith(Range<T> other)
-        {
-            return elements.Any(element => other.OverlapsWith(element));
-        }
+            public override bool Includes(T value)
+            {
+                return elements.Any(element => element.Includes(value));
+            }
 
-        public override bool OverlapsWith(SingleRange<T> other)
-        {
-            return elements.Any(element => other.OverlapsWith(element));
-        }
+            public override bool OverlapsWith(Range<T> other)
+            {
+                return elements.Any(element => other.OverlapsWith(element));
+            }
 
-        public override Range<T> Add(Range<T> other)
-        {
-            throw new NotImplementedException();
-        }
+            protected override bool OverlapsWith(SingleRange other)
+            {
+                return elements.Any(element => other.OverlapsWith(element));
+            }
 
-        public override Range<T> Intersect(Range<T> other)
-        {
-            throw new NotImplementedException();
-        }
+            public override Range<T> Add(Range<T> other)
+            {
+                throw new NotImplementedException();
+            }
 
-        public override Range<T> Complement
-        {
-            get { throw new NotImplementedException(); }
-        }
+            public override Range<T> Intersect(Range<T> other)
+            {
+                throw new NotImplementedException();
+            }
 
-        public override Range<T> GreaterThan(T value)
-        {
-            throw new NotImplementedException();
-        }
+            public override Range<T> Complement
+            {
+                get { throw new NotImplementedException(); }
+            }
 
-        public override Range<T> GreaterThanOrEquals(T value)
-        {
-            throw new NotImplementedException();
-        }
+            public override Range<T> GreaterThan(T value)
+            {
+                throw new NotImplementedException();
+            }
 
-        public override Range<T> LessThan(T value)
-        {
-            throw new NotImplementedException();
-        }
+            public override Range<T> GreaterThanOrEquals(T value)
+            {
+                throw new NotImplementedException();
+            }
 
-        public override Range<T> LessThanOrEquals(T value)
-        {
-            throw new NotImplementedException();
-        }
+            public override Range<T> LessThan(T value)
+            {
+                throw new NotImplementedException();
+            }
 
-        public override bool IsEmpty
-        {
-            get { throw new NotImplementedException(); }
-        }
+            public override Range<T> LessThanOrEquals(T value)
+            {
+                throw new NotImplementedException();
+            }
 
-        public override bool IsUniversal
-        {
-            get { throw new NotImplementedException(); }
-        }
+            public override bool IsEmpty
+            {
+                get { throw new NotImplementedException(); }
+            }
 
-        public override string ToString()
-        {
-            return string.Join(",", elements.Select(element => element.ToString()).ToArray());
+            public override bool IsUniversal
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public override string ToString()
+            {
+                return string.Join(",", elements.Select(element => element.ToString()).ToArray());
+            }
         }
     }
 }

2edbfa66e9525f76aed7bfe67fd1e0098aabdb3c
Miura Takuma <mueller@inter7.jp>
2012-09-26 23:46:17 +0900

    SingleRange, ComplexRangeをクラス内クラスにして隠蔽

--- ValueRange/Range.cs
+++ ValueRange/Range.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace ValueRange
 {
-    public abstract class Range<T> where T : IComparable<T>
+    public abstract partial class Range<T> where T : IComparable<T>
     {
         public static Range<T> operator <(T left, Range<T> right) { return right.GreaterThan(left); }
         public static Range<T> operator >(T left, Range<T> right) { return right.LessThan(left); }
@@ -30,7 +30,7 @@ namespace ValueRange
 
         public abstract bool Includes(T value);
         public abstract bool OverlapsWith(Range<T> other);
-        public abstract bool OverlapsWith(SingleRange<T> other);
+        protected abstract bool OverlapsWith(SingleRange other);
         public abstract Range<T> Add(Range<T> other);
         public virtual Range<T> Subtract(Range<T> other) { return other.Complement; }
         public abstract Range<T> Intersect(Range<T> other);
@@ -50,7 +50,7 @@ namespace ValueRange
 
             public override bool Includes(T value) { return false; }
             public override bool OverlapsWith(Range<T> other) { return false; }
-            public override bool OverlapsWith(SingleRange<T> other) { return false; }
+            protected override bool OverlapsWith(SingleRange other) { return false; }
             public override Range<T> Add(Range<T> other) { return other; }
             public override Range<T> Subtract(Range<T> other) { return this; }
             public override Range<T> Intersect(Range<T> other) { return this; }
@@ -72,15 +72,15 @@ namespace ValueRange
 
             public override bool Includes(T value) { return true; }
             public override bool OverlapsWith(Range<T> other) { return true; }
-            public override bool OverlapsWith(SingleRange<T> other) { return true; }
+            protected override bool OverlapsWith(SingleRange other) { return true; }
             public override Range<T> Add(Range<T> other) { return this; }
             public override Range<T> Intersect(Range<T> other) { return other; }
             public override Range<T> Complement { get { return EmptyRange.Instance; } }
 
-            public override Range<T> GreaterThan(T value) { return SingleRange<T>.ThatGreaterThan(value); }
-            public override Range<T> GreaterThanOrEquals(T value) { return SingleRange<T>.ThatGreaterThanOrEquals(value); }
-            public override Range<T> LessThan(T value) { return SingleRange<T>.ThatLessThan(value); }
-            public override Range<T> LessThanOrEquals(T value) { return SingleRange<T>.ThatLessThanOrEquals(value); }
+            public override Range<T> GreaterThan(T value) { return SingleRange.ThatGreaterThan(value); }
+            public override Range<T> GreaterThanOrEquals(T value) { return SingleRange.ThatGreaterThanOrEquals(value); }
+            public override Range<T> LessThan(T value) { return SingleRange.ThatLessThan(value); }
+            public override Range<T> LessThanOrEquals(T value) { return SingleRange.ThatLessThanOrEquals(value); }
 
             public override bool IsEmpty { get { return false; } }
             public override bool IsUniversal { get { return true; } }
--- ValueRange/SingleRange.cs
+++ ValueRange/SingleRange.cs
@@ -5,129 +5,132 @@ using System.Text;
 
 namespace ValueRange
 {
-    public class SingleRange<T> : Range<T> where T : IComparable<T>
+    public partial class Range<T> where T : IComparable<T>
     {
-        readonly BoundValue<T> lower, upper;
-
-        private SingleRange(BoundValue<T> lower, BoundValue<T> upper)
+        protected class SingleRange : Range<T>
         {
-            this.lower = lower;
-            this.upper = upper;
-        }
+            readonly BoundValue<T> lower, upper;
 
-        public static SingleRange<T> ThatGreaterThan(T value)
-        {
-            return new SingleRange<T>(BoundValue<T>.UpperBound(value), BoundValue<T>.PositiveInfinity);
-        }
-
-        public static SingleRange<T> ThatGreaterThanOrEquals(T value)
-        {
-            return new SingleRange<T>(BoundValue<T>.LowerBound(value), BoundValue<T>.PositiveInfinity);
-        }
-
-        public static SingleRange<T> ThatLessThan(T value)
-        {
-            return new SingleRange<T>(BoundValue<T>.NegativeInfinity, BoundValue<T>.LowerBound(value));
-        }
-
-        public static SingleRange<T> ThatLessThanOrEquals(T value)
-        {
-            return new SingleRange<T>(BoundValue<T>.NegativeInfinity, BoundValue<T>.UpperBound(value));
-        }
-
-        public override bool Includes(T value)
-        {
-            return lower <= BoundValue<T>.UpperBound(value) && BoundValue<T>.LowerBound(value) <= upper;
-        }
-
-        public override bool OverlapsWith(Range<T> other)
-        {
-            return other.OverlapsWith(this);
-        }
-
-        public override bool OverlapsWith(SingleRange<T> other)
-        {
-            return lower <= other.upper && other.lower <= upper;
-        }
-
-        public override Range<T> Add(Range<T> other)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Range<T> Intersect(Range<T> other)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Range<T> Complement
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public override Range<T> GreaterThan(T value)
-        {
-           var newBound = BoundValue<T>.UpperBound(value);
-            if (upper < newBound) return EmptyRange.Instance;
-            return new SingleRange<T>(newBound.Max(lower), upper);
-        }
-
-        public override Range<T> GreaterThanOrEquals(T value)
-        {
-            var newBound = BoundValue<T>.LowerBound(value);
-            if (upper < newBound) return EmptyRange.Instance;
-            return new SingleRange<T>(newBound.Max(lower), upper);
-        }
-
-        public override Range<T> LessThan(T value)
-        {
-            var newBound = BoundValue<T>.LowerBound(value);
-            if (newBound < lower) return EmptyRange.Instance;
-            return new SingleRange<T>(lower, newBound.Min(upper));
-        }
-
-        public override Range<T> LessThanOrEquals(T value)
-        {
-            var newBound = BoundValue<T>.UpperBound(value);
-            if (newBound < lower) return EmptyRange.Instance;
-            return new SingleRange<T>(lower, newBound.Min(upper));
-        }
-
-        public override bool IsEmpty
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public override bool IsUniversal
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public override string ToString()
-        {
-            string result = "";
-
-            if (lower is BoundValue<T>.LowerBoundValue)
+            private SingleRange(BoundValue<T> lower, BoundValue<T> upper)
             {
-                result +=  "[" + lower.ToString();
-            }
-            else
-            {
-                result += "(" + lower.ToString();
+                this.lower = lower;
+                this.upper = upper;
             }
 
-            result += ",";
-
-            if (upper is BoundValue<T>.UpperBoundValue)
+            public static SingleRange ThatGreaterThan(T value)
             {
-                result += upper.ToString() + "]";
-            }
-            else
-            {
-                result += upper.ToString() + ")";
+                return new SingleRange(BoundValue<T>.UpperBound(value), BoundValue<T>.PositiveInfinity);
             }
 
-            return result;
+            public static SingleRange ThatGreaterThanOrEquals(T value)
+            {
+                return new SingleRange(BoundValue<T>.LowerBound(value), BoundValue<T>.PositiveInfinity);
+            }
+
+            public static SingleRange ThatLessThan(T value)
+            {
+                return new SingleRange(BoundValue<T>.NegativeInfinity, BoundValue<T>.LowerBound(value));
+            }
+
+            public static SingleRange ThatLessThanOrEquals(T value)
+            {
+                return new SingleRange(BoundValue<T>.NegativeInfinity, BoundValue<T>.UpperBound(value));
+            }
+
+            public override bool Includes(T value)
+            {
+                return lower <= BoundValue<T>.UpperBound(value) && BoundValue<T>.LowerBound(value) <= upper;
+            }
+
+            public override bool OverlapsWith(Range<T> other)
+            {
+                return other.OverlapsWith(this);
+            }
+
+            protected override bool OverlapsWith(SingleRange other)
+            {
+                return lower <= other.upper && other.lower <= upper;
+            }
+
+            public override Range<T> Add(Range<T> other)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override Range<T> Intersect(Range<T> other)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override Range<T> Complement
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public override Range<T> GreaterThan(T value)
+            {
+                var newBound = BoundValue<T>.UpperBound(value);
+                if (upper < newBound) return EmptyRange.Instance;
+                return new SingleRange(newBound.Max(lower), upper);
+            }
+
+            public override Range<T> GreaterThanOrEquals(T value)
+            {
+                var newBound = BoundValue<T>.LowerBound(value);
+                if (upper < newBound) return EmptyRange.Instance;
+                return new SingleRange(newBound.Max(lower), upper);
+            }
+
+            public override Range<T> LessThan(T value)
+            {
+                var newBound = BoundValue<T>.LowerBound(value);
+                if (newBound < lower) return EmptyRange.Instance;
+                return new SingleRange(lower, newBound.Min(upper));
+            }
+
+            public override Range<T> LessThanOrEquals(T value)
+            {
+                var newBound = BoundValue<T>.UpperBound(value);
+                if (newBound < lower) return EmptyRange.Instance;
+                return new SingleRange(lower, newBound.Min(upper));
+            }
+
+            public override bool IsEmpty
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public override bool IsUniversal
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public override string ToString()
+            {
+                string result = "";
+
+                if (lower is BoundValue<T>.LowerBoundValue)
+                {
+                    result += "[" + lower.ToString();
+                }
+                else
+                {
+                    result += "(" + lower.ToString();
+                }
+
+                result += ",";
+
+                if (upper is BoundValue<T>.UpperBoundValue)
+                {
+                    result += upper.ToString() + "]";
+                }
+                else
+                {
+                    result += upper.ToString() + ")";
+                }
+
+                return result;
+            }
         }
     }
 }
